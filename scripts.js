9d1dadafa80eb37745925a93ffdcbea51bb2658c
eempc <evc484@gmail.com>
2019-09-06 20:17:51 +0100

    contact form basic validation

--- scripts.js
+++ scripts.js
@@ -3,3 +3,35 @@ const externalLinks = document.getElementsByClassName("nav-icon");
 for (let i = 0; i < externalLinks.length; i++) {
     externalLinks[i].setAttribute('target', '_blank');
 }
+
+const submitButton = document.getElementById("submit-contact-form").addEventListener("click", validateFields);
+
+function validateFields() {
+    const nameInput = document.getElementById("fullname");
+    const nameMessage = document.getElementById("required-fullname");
+
+    if (!nameInput.checkValidity()) {
+        nameMessage.innerHTML = nameInput.validationMessage;
+    } else {
+        nameMessage.innerHTML = "Okay";
+    }
+
+    const emailInput = document.getElementById("emailaddress");
+    const emailMessage = document.getElementById("required-email");
+
+    if (!emailInput.checkValidity()) {
+        emailMessage.innerHTML = emailInput.validationMessage;
+    } else {
+        emailMessage.innerHTML = "Okay";
+    }
+
+    const messageInput = document.getElementById("messagebox");
+    const messageBoxMessage = document.getElementById("required-message");
+
+    if (!messageInput.checkValidity()) {
+        messageBoxMessage.innerHTML = messageInput.validationMessage;
+    } else {
+        messageBoxMessage.innerHTML = "Okay";
+    }
+
+}
