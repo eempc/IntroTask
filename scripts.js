87fd7785c87c76289ebf8c30f10462f6bf5c3505
eempc <evc484@gmail.com>
2019-09-07 12:19:53 +0100

    basic video link

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ for (let i = 0; i < externalLinks.length; i++) {
 
 // Placeholder javascript form validation script in lieu of POSTing
 
-const submitButton = document.getElementById("submit-contact-form").addEventListener("click", validateFields);
+//const submitButton = document.getElementById("submit-contact-form").addEventListener("click", validateFields);
 
 function validateFields() {
     const nameInput = document.getElementById("fullname");
@@ -39,29 +39,3 @@ function validateFields() {
     }
 }
 
-// Barebones Slideshow script
-
-let currentSlide = 0;
-showSlide(currentSlide);
-
-function showSlide(x) {
-    // Get the slides into an array
-    const slides = document.getElementsByClassName("slide");
-
-    // Ensure index is not out of bounds
-    if (x >= slides.length) currentSlide = 0;
-    if (x < 0) currentSlide = slides.length - 1;
-
-    // Hide all slides
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    // Then show the current slide
-    slides[currentSlide].style.display = "block";
-
-}
-
-function navigateSlides(x) {
-    showSlide(currentSlide += x);
-}
---
+++ slides.js
@@ -0,0 +1,26 @@
+// Barebones Slideshow script
+
+let currentSlide = 0;
+showSlide(currentSlide);
+
+function showSlide(x) {
+    // Get the slides into an array
+    const slides = document.getElementsByClassName("slide");
+
+    // Ensure index is not out of bounds
+    if (x >= slides.length) currentSlide = 0;
+    if (x < 0) currentSlide = slides.length - 1;
+
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Then show the current slide
+    slides[currentSlide].style.display = "block";
+
+}
+
+function navigateSlides(x) {
+    showSlide(currentSlide += x);
+}
